9a3d4e5fffbf95436271ddfe83d917210ac236b6
salai <uchiasadara6@gmail.com>
2024-09-19 00:18:21 +0630

    added some features and fix some bugs

--- Periodic_Table.h
+++ Periodic_Table.h
@@ -7,10 +7,23 @@
 
 #include <stdio.h>
 #include "stdlib.h"
+#include "string.h"
 #define Max_Table_Size 6
 
 int n,m,a,Exit,Exi;
 char key[Max_Table_Size][30] = {"Name","Symbolic","Atomic Number","Electronic Configuration","Discovered By","Charge"};
+char Hydrogen [Max_Table_Size][20] = {"Hydrogen","H","1","1s¹","Henry Cavendish","+1"};
+char Helium [Max_Table_Size][20] = {"Helium","He","2","1s²","Pierre Janssen","0"};
+char Lithium [Max_Table_Size][25] = {"Lithium","Li","3","[He] 2s¹","Johan August Arfwedson","+1"};
+char Beryllium [Max_Table_Size][25] = {"Beryllium","Be","4","[He] 2s²","Louis-Nicolas Vauquelin","+2"};
+char Boron [Max_Table_Size][25] = {"Boron","B","5","[He] 2s² 2p¹","Joseph Louis Gay-Lussac","+3"};
+char Carbon [Max_Table_Size][20] = {"Carbon","c","6","[He] 2s² 2p²","Ancient","±4"};
+char Nitrogen [Max_Table_Size][20] = {"Nitrogen","N","7","[He] 2s² 2p³","Daniel Rutherford","-3"};
+char Oxygen [Max_Table_Size][20] = {"Oxygen","O","8","[He] 2s² 2p⁴","Joseph Priestley","-2"};
+char Fluorine [Max_Table_Size][22] = {"Fluorine","F","9","[He] 2s² 2p⁵","Henri Moissan","-1"};
+char Neon [Max_Table_Size][20] = {"Neon","Ne","10","[He] 2s² 2p⁶","William Ramsay","0"};
+
+
 //Function declear
 void menu();
 void Showtable();
@@ -37,30 +50,30 @@ void SearchElement(){
         scanf("%d",&m);
         Showtable();
     } else if(n == 2)
-    {   char choice;
+    {   char choice[4];
         printf("Do you want to exit? (Yes/No)\n");
         scanf("%s",&choice);
 
-        if(choice == "yes" || choice == "Yes"){
+        if (strcmp(choice, "yes") == 0 || strcmp(choice, "Yes") == 0) {
             printf("\nThe periodic table has closed.");
             exit(0);
-        }
-        else if(choice == "no" || choice == "No")
-        {
-            printf("\nPeriodic table did not closed and you can continue to learn more about the elements\n");
+        } else if (strcmp(choice, "no") == 0 || strcmp(choice, "No") == 0) {
+            printf("\nPeriodic table did not close and you can continue to learn more about the elements\n");
             menu();
+        } else {
+            printf("Invalid input! You should enter Yes or No.\n");
+            printf("System was broke-out!!!\n");
         }
     }
 }
 
 
 void Showtable(){
-    if(m ==3){
         printf("\n[+]Enter the atomic number of the element to be searched: ");
-        scanf("%d\n",&a);
+        scanf("%d",&a);
 
         if(a == 1)
-        {  char Hydrogen [Max_Table_Size][20] = {"Hydrogen","H","1","1s¹","Henry Cavendish","+1"};
+        {
             // Loop through the array and print each fruit
             printf("\n#####\t#####\t#####\n");
             for (int i = 0; i < Max_Table_Size; i++) {
@@ -68,94 +81,105 @@ void Showtable(){
             }
             printf("\n#####\t#####\t#####\n");
 
+            menu();
+
         } else if (a == 2)
         {
-            char Helium [Max_Table_Size][20] = {"Helium","He","2","1s²","Pierre Janssen","0"};
+
             // Loop through the array and print each fruit
             printf("\n#####\t#####\t#####\n");
             for (int i = 0; i < Max_Table_Size; i++) {
                 printf("%s : %s\n", key[i], Helium[i]);
             }
             printf("\n#####\t#####\t#####\n");
+            menu();
         } else if (a == 3)
         {
-            char Lithium [Max_Table_Size][25] = {"Lithium","Li","3","[He] 2s¹","Johan August Arfwedson","+1"};
+
             // Loop through the array and print each fruit
             printf("\n#####\t#####\t#####\n");
             for (int i = 0; i < Max_Table_Size; i++) {
                 printf("%s : %s\n", key[i], Lithium[i]);
             }
             printf("\n#####\t#####\t#####\n");
+            menu();
         }else if (a == 4)
         {
-            char Hydrogen [Max_Table_Size][20] = {"Hydrogen","H","1","1s^1","Henry Cavendish","+1"};
+
             // Loop through the array and print each fruit
             printf("\n#####\t#####\t#####\n");
             for (int i = 0; i < Max_Table_Size; i++) {
-                printf("%s : %s\n", key[i], Hydrogen[i]);
+                printf("%s : %s\n", key[i], Beryllium[i]);
             }
             printf("\n#####\t#####\t#####\n");
+            menu();
         }else if (a == 5)
         {
-            char Hydrogen [Max_Table_Size][20] = {"Hydrogen","H","1","1s^1","Henry Cavendish","+1"};
+
             // Loop through the array and print each fruit
             printf("\n#####\t#####\t#####\n");
             for (int i = 0; i < Max_Table_Size; i++) {
-                printf("%s : %s\n", key[i], Hydrogen[i]);
+                printf("%s : %s\n", key[i], Boron[i]);
             }
             printf("\n#####\t#####\t#####\n");
+            menu();
         }else if (a == 6)
         {
-            char Hydrogen [Max_Table_Size][20] = {"Hydrogen","H","1","1s^1","Henry Cavendish","+1"};
+
             // Loop through the array and print each fruit
             printf("\n#####\t#####\t#####\n");
             for (int i = 0; i < Max_Table_Size; i++) {
-                printf("%s : %s\n", key[i], Hydrogen[i]);
+                printf("%s : %s\n", key[i], Carbon[i]);
             }
             printf("\n#####\t#####\t#####\n");
+            menu();
         }else if (a == 7)
         {
-            char Hydrogen [Max_Table_Size][20] = {"Hydrogen","H","1","1s^1","Henry Cavendish","+1"};
+
             // Loop through the array and print each fruit
             printf("\n#####\t#####\t#####\n");
             for (int i = 0; i < Max_Table_Size; i++) {
-                printf("%s : %s\n", key[i], Hydrogen[i]);
+                printf("%s : %s\n", key[i], Nitrogen[i]);
             }
             printf("\n#####\t#####\t#####\n");
+            menu();
         } else if (a == 8)
         {
-            char Hydrogen [Max_Table_Size][20] = {"Hydrogen","H","1","1s^1","Henry Cavendish","+1"};
+
             // Loop through the array and print each fruit
             printf("\n#####\t#####\t#####\n");
             for (int i = 0; i < Max_Table_Size; i++) {
-                printf("%s : %s\n", key[i], Hydrogen[i]);
+                printf("%s : %s\n", key[i], Oxygen[i]);
             }
             printf("\n#####\t#####\t#####\n");
+            menu();
         }else if (a == 9)
         {
-            char Hydrogen [Max_Table_Size][20] = {"Hydrogen","H","1","1s^1","Henry Cavendish","+1"};
+
             // Loop through the array and print each fruit
             printf("\n#####\t#####\t#####\n");
             for (int i = 0; i < Max_Table_Size; i++) {
-                printf("%s : %s\n", key[i], Hydrogen[i]);
+                printf("%s : %s\n", key[i], Fluorine[i]);
             }
             printf("\n#####\t#####\t#####\n");
+            menu();
         }else if (a == 10)
         {
-            char Hydrogen [Max_Table_Size][20] = {"Hydrogen","H","1","1s^1","Henry Cavendish","+1"};
+
             // Loop through the array and print each fruit
             printf("\n#####\t#####\t#####\n");
             for (int i = 0; i < Max_Table_Size; i++) {
-                printf("%s : %s\n", key[i], Hydrogen[i]);
+                printf("%s : %s\n", key[i], Neon[i]);
             }
             printf("\n#####\t#####\t#####\n");
+            menu();
         }
         else{
             printf("\nPlease enter a certain number for search element!\n");
             SearchElement();
         }
     }
-}
+
 
 
 
